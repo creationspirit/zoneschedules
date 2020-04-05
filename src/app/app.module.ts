c85d632a646cea2c6cb44fa7d44d0e58af220f16
Andrija Perusic <aperusic18@gmail.com>
2020-04-05 18:53:21 +0200

    Add httpClient to appmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ZoneOverviewComponent } from './components/zone-overview/zone-overview.component';
+import { ScheduleEditComponent } from './components/schedule-edit/schedule-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ZoneOverviewComponent,
+    ScheduleEditComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
