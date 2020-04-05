e5ad2f6e4361181b72d8de006bcffe5ee6221ead
Andrija Perusic <aperusic18@gmail.com>
2020-04-05 18:52:05 +0200

    Add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { ZoneOverviewComponent } from './components/zone-overview/zone-overview.component';
+import { ScheduleEditComponent } from './components/schedule-edit/schedule-edit.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: '/overview', pathMatch: 'full' },
+  { path: 'overview', component: ZoneOverviewComponent },
+  { path: 'new', component: ScheduleEditComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
